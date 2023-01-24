d0ae241f6dadc54a9d86c47b249304b23a244617
grossmj <grossmj@gns3.net>
2023-01-24 15:48:16 +0800

    Release v1.0.0b4

--- gns3_webclient_pack/version.py
+++ gns3_webclient_pack/version.py
@@ -23,7 +23,7 @@
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
 
-__version__ = "1.0.0dev4"
+__version__ = "1.0.0b4"
 __version_info__ = (1, 0, 0, -99)
 
 #  Add the commit ID if this is a development version
@@ -33,6 +33,6 @@ if "dev" in __version__:
         import subprocess
         if os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".git")):
             r = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode().strip("\n")
-            __version__ += "-" + r
+            __version__ += "+" + r
     except Exception as e:
         print(e)
